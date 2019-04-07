ff5997f8ab5cc6d07335e975409297d27b4f1a2f
jinsukang <240985757@qq.com>
2019-04-07 22:57:49 +0800

    source code

---
+++ PPDotNetDemo/Controllers/PayPalController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace PPDotNetDemo.Controllers
+{
+	public class PayPalController : Controller
+	{
+		// GET: PP
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult Success(string orderId, string givenName, string surName)
+		{
+			ViewBag.OrderId = orderId;
+			ViewBag.GivenName = givenName;
+			ViewBag.SurName = surName;
+			return View("Success");
+		}
+	}
+}
